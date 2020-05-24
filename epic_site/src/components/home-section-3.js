6cebbd9e6f5dba231f85e9423dd95b287dad8dd8
Diana Shao <dianashao@utexas.edu>
2020-05-23 21:09:19 -0500

    add section 3 labels

--- epic_site/src/components/home-section-3.js
+++ epic_site/src/components/home-section-3.js
@@ -3,8 +3,6 @@ import './../styles/home.css'
 import img1 from '../images/christmas-potluck.jpeg'
 import img2 from '../images/christmas-potluck.jpeg'
 
-//creates a carousel of images that are passed in through props
-//will fill to 100% of whatever container it is placed in
 
 class HomeSection3 extends Component{
     constructor(props){
@@ -17,14 +15,20 @@ class HomeSection3 extends Component{
                 <div className="Section-Three">
                     <div className="box">
                       <img src={img1}></img>
+                      <div className="label">
+                        <h4>Small Group</h4>
+                      </div>
                       <div className="text-box">
                         <h3>It's never too late to join a small group!</h3>
                       </div>
                     </div>
                     <div className="box">
                       <img src={img2}></img>
+                      <div className="label">
+                        <h4>Large Group</h4>
+                      </div>
                       <div className="text-box">
-                        <h3>It's never too late to join a small group!</h3>
+                        <h3>Come meet people at large group!</h3>
                       </div>
                     </div>
                 </div>

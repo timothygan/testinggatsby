724e50fc7002ad9d22f3f1eabb8da5e0e6262a7d
Diana Shao <dianashao@utexas.edu>
2020-06-01 21:41:22 -0500

    side nav menu

--- epic_site/src/components/header.js
+++ epic_site/src/components/header.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react"
 import { Link } from 'gatsby'
 import logo from './../images/bw-epic-logo.png'
 import './../styles/header.css'
+import '../styles/menu-button.css'
 
 class Header extends Component{
     constructor(props){
@@ -20,6 +21,19 @@ class Header extends Component{
       this.setState({
         toggled: !this.state.toggled
       });
+      if(!this.state.toggled) {
+        this.showSideNav();
+      } else {
+        this.hideSideNav();
+      }
+    }
+
+    showSideNav() {
+      document.getElementById("sidenav").style.width = "20%";
+    }
+
+    hideSideNav() {
+      document.getElementById("sidenav").style.width = "0%";
     }
 
     handleScroll = () => {
@@ -38,8 +52,7 @@ class Header extends Component{
 
         return(
             <Fragment>
-                <header onScroll={ this.handleScroll }
-                          className={ this.state.scrolled ? "scrolled" : "Banner" }
+                <header className={ this.state.scrolled ? "scrolled" : "Banner" }
                           id="Banner">
                     <Link to={'/'}><img src = {logo} alt="epic movement logo"></img></Link>
                     <Link to={'/'} className={"Header-Title-Link"}>
@@ -54,7 +67,18 @@ class Header extends Component{
                         <div className="bar2"></div>
                         <div className="bar3"></div>
                     </div>
-
+                    <div id="sidenav" >
+                      <a href="#">About</a>
+                      <a href="#">Calendar</a>
+                      <a href="#">Large Group</a>
+                      <a href="#">Small Group</a>
+                      <a href="#">Church Rides</a>
+                      <a href="#">Go Team</a>
+                      <a href="#">Summer Missions</a>
+                      <a href="#">Blog</a>
+                      <a href="#">Exploring Faith</a>
+                      <a href="#">Contact Us</a>
+                    </div>
                 </header>
 
             </Fragment>

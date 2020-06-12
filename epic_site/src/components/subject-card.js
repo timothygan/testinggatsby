6cba3e6e55362804789c4d3ad72571993d442f37
Timothy Gan <timothygan@utexas.edu>
2020-06-12 15:16:46 -0500

    made graphics in section4 scale

--- epic_site/src/components/subject-card.js
+++ epic_site/src/components/subject-card.js
@@ -16,21 +16,20 @@ class SubjectCard extends Component{
         return(
             <Fragment>
                 <div className="card">
-				<div className="hoverText">
-					{this.props.description}
-				</div>
-                <div className="graphic">
-					{this.props.graphic}
-    			</div>
-
-                        <AnimatedButton className="AnimatedButton"
-                                        hoverBackground='rgba(255,255,255,1)'
-                                        hoverTextColor='rgba(255, 120, 100)'
-                                        width='50%'
-                                        height="50%">
-                                            {this.props.buttonText} 
-                        </AnimatedButton>
+                    <div className="hoverText">
+                        {this.props.description}
                     </div>
+                    <div className="graphic">
+                        {this.props.graphic}
+                    </div>
+                    <AnimatedButton className="AnimatedButton"
+                                    hoverBackground='rgba(255,255,255,1)'
+                                    hoverTextColor='rgba(255, 120, 100)'
+                                    width='50%'
+                                    height="50%">
+                                        {this.props.buttonText} 
+                    </AnimatedButton>
+                </div>
                     
             </Fragment>
                 

0bd7a4b68c62787853b2ba1a73a3bbb44f83deb0
Diana Shao <dianashao@utexas.edu>
2020-06-13 17:46:35 -0500

    contact-us page

--- epic_site/src/pages/contact-us.js
+++ epic_site/src/pages/contact-us.js
@@ -2,7 +2,12 @@ import React, { Component, Fragment } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import EventHeader from "../components/event-header";
+import Form from "../components/form";
+import '../styles/contact-us.css';
+
 import img from '../images/contact-us.jpg';
+import fb from '../images/fb.png';
+import insta from '../images/insta.png';
 
 class ContactUs extends Component{
   constructor(props){
@@ -14,6 +19,22 @@ class ContactUs extends Component{
       <Fragment>
         <Header/>
         <EventHeader title={'Contact Us'} image={img}/>
+        <div className={'bottom-section'} id={'contact-bottom-section'}>
+          <Form url={'https://forms.gle/1PwZbdnAmiuPiAh28'}></Form>
+          <div id={'social-container'}>
+          <div id={'social-media'}>
+            <a href={'https://www.facebook.com/groups/texasepic/'} target={'_blank'}>
+              <img className={'social-icon'} id={'fb'} src={fb}></img>
+            </a>
+            <a href={'https://www.instagram.com/texasepic/'} target={'_blank'}>
+              <img className={'social-icon'} id={'insta'} src={insta}></img>
+            </a>
+          </div>
+          <div id={'epic-email-container'}>
+            <a id={'epic-email'} href={'mailto:texasepic@gmail.com'}>texasepic@gmail.com</a>
+          </div>
+        </div>
+        </div>
         <Footer/>
       </Fragment>
     )
